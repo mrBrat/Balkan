9c64bed525bee862143d961f0a7f0bc609a4ffd9
nihad08 <nihad08@live.com>
2014-01-03 13:05:12 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 API.chatLog("Running Balkan Party RoomScript!", true);
 API.chatLog("version 0.015 (030114)", true);
-API.chatLog("* :santa: HAPPY NEW YEAR 2014 :santa: *", true);
+API.chatLog(":santa: HAPPY NEW YEAR 2014 :santa:", true);
 function initEnvironment(){
 	if (plugCubed === undefined) {
 		$.getScript('http://alpha.plugCubed.net/plugCubed.js'); 
@@ -25,12 +25,12 @@ RMEnhancedModel = require('a96fc/ff0b8/e72a9').extend({
             autowoot         : false
     },
     initCSS: function() {
-		$(".background").find('img').attr('src','http://i.imgur.com/xcPLupP.png');
+		$(".background").find('img').attr('src','http://i.imgur.com/qbovOzC.png');
         $('head').append('<link rel="stylesheet" type="text/css" href="https://dl.dropboxusercontent.com/s/xrc8z5doey7y4zc/style.css">');
 		$('body').append('<div id="tooltip_custom" class="right" style="top: 28px; right: 120px; visibility: hidden;"><span>AutoWoot Enable/Disable</span><div class="corner"></div></div>');
 		$('body').append('<div id="tooltip_custom1" class="right" style="top: 28px; right: 70px; visibility: hidden;"><span>AutoJoin Enable/Disable</span><div class="corner"></div></div>');
 		$('#room').find('canvas').filter(function() { return $(this).css('opacity') < 1; }).remove();
-		$('#room').append('<div class="is-leave" style="margin-top: 300px; text-align: center;"><a href="https://www.facebook.com/groups/547885508611462/" target="_blank"><img src="http://i.imgur.com/eoeofSU.png"></a></div>');
+		$('#room').append('<div class="is-leave" style="margin-top: 300px; text-align: center;"><a href="https://www.facebook.com/groups/547885508611462/" target="_blank"><img src="http://i.imgur.com/F1p1ZET.png"></a></div>');
 		$('#dj-booth').append('<div id="rmbooth" style="background-image: url(http://i.imgur.com/tg0qpxC.png);"></div>');
 		$('#chat-header').append('<div id="autowoot-button" class="chat-header-button" onMouseOver="show(\'tooltip_custom\')" onMouseOut="hide(\'tooltip_custom\')" onClick="woot()"><i class="icon icon-autowoot-off"></i></div>');
 	},
