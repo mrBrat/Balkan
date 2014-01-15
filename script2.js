23520ea55dae462c6b10e05b5ca521d8def66cd9
nihad08 <nihad08@live.com>
2014-01-15 18:18:03 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 API.chatLog("Balkan Party RoomScript!", true);
-API.chatLog("running version 0.016 (150114) :white_check_mark:", true);
+API.chatLog("running version 0.016 (150114) :ballot_box_with_check:", true);
 function initEnvironment(){
        if (plugCubed === undefined) {
         //$.getScript('http://alpha.plugCubed.net/plugCubed.js'); 
