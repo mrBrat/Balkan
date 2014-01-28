35a036a967eca62f7faee47954451c3e08440dd6
nihad08 <nihad08@live.com>
2014-01-28 15:56:28 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -21,7 +21,7 @@ RMEnhancedModel = require('abb7c/c2b23/a33aa').extend({
         };
     },
     initCSS: function() {
-                $(".background").find('img').attr('src','http://i.imgur.com/264isaR.png');
+                //$(".background").find('img').attr('src','http://i.imgur.com/264isaR.png');
         $('head').append('<link rel="stylesheet" type="text/css" href="https://dl.dropboxusercontent.com/s/xrc8z5doey7y4zc/style.css">');
                 $('body').append('<div id="tooltip_custom" class="right" style="top: 28px; right: 120px; visibility: hidden;"><span>AutoWoot Enable/Disable</span><div class="corner"></div></div>');
                 $('body').append('<div id="tooltip_custom1" class="right" style="top: 28px; right: 70px; visibility: hidden;"><span>AutoJoin Enable/Disable</span><div class="corner"></div></div>');
