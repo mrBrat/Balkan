06d3d486dbf35e71a8e55e07a83ec16fc63c7446
nihad08 <nihad08@live.com>
2014-02-21 23:44:54 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -21,7 +21,7 @@ RMEnhancedModel = require('c447a/b3359/e3636').extend({
         };
     },
     initCSS: function() {
-                $("body").prepend('<img src="http://i.imgur.com/G6oFJB3.gif" id="BALKAN">');
+                $("body").prepend('<img src="http://i.imgur.com/bU8igZK.gif" id="BALKAN">');
                 $("body").prepend('<style> #balkan { position:absolute; top: 50px; left: 30px; } </style>');
                 $(".background").find('img').attr('src','http://i.imgur.com/yuPIW5I.png');
         $('head').append('<link rel="stylesheet" type="text/css" href="https://dl.dropboxusercontent.com/s/xrc8z5doey7y4zc/style.css">');
